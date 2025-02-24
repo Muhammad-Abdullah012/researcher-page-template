9d064dd1f76deaa9d58c884cfb8161221dcb509c
Muhammad Abdullah <68374783+Muhammad-Abdullah012@users.noreply.github.com>
2025-02-24 13:28:36 +0500

    fix: build issues in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+console.log("base url VITE_BASE => ", process.env.VITE_BASE);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/researcher-page-template/"
+  base: process.env.VITE_BASE || '/',
 })
