ccb9ef98d9461b3ea1977c3e76a0454c96945e71
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2025-02-27 18:49:54 +0500

    Revert "feat: dynamic publications data"
    This reverts commit 63b865eb56e2e86fcf84a32fe882b232ba3eb525.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
   import ThemeToggle from "./lib/ThemeToggle.svelte";
-  import { fetchCitations } from "./lib/utils/fetchCitations";
 
-  const rssFeedUrl =
-    "https://scholar.google.com/citations?user=a7COTC8AAAAJ&hl=en&cstart=0&pagesize=100";
-
-  let isLoading = true;
   const researcher = {
     name: "Dr. Muhammad Ahsan Iqbal",
     title: "Postdoc MSCA (Chemical and Materials)",
@@ -41,7 +36,128 @@
         year: "2010 - 2014",
       },
     ],
-    publications: [] as any[],
+    publications: [
+      {
+        title:
+          "Effect of synthesis conditions on the controlled growth of MgAl-LDH corrosion resistance film: structure and corrosion resistance properties",
+        journal: "Coatings",
+        year: "2019",
+        doi: "10.3390/coatings9010030",
+        citations: 64,
+      },
+      {
+        title:
+          "The effect of the surface morphologies on the corrosion resistance of in situ growth MgAl-LDH based conversion film on AA6082",
+        journal: "Surface and Coatings Technology",
+        year: "2018",
+        doi: "10.1016/j.surfcoat.2018.08.013",
+        citations: 54,
+      },
+      {
+        title:
+          "Layered double hydroxide protective films developed on aluminum and aluminum alloys: synthetic methods and anti-corrosion mechanisms",
+        journal: "Coatings",
+        year: "2020",
+        doi: "10.3390/coatings10040428",
+        citations: 52,
+      },
+      {
+        title:
+          "In situ growth of a CaAl-NO3--layered double hydroxide film directly on an aluminum alloy for corrosion resistance",
+        journal: "Dalton Transactions",
+        year: "2020",
+        doi: "10.1039/C9DT04595A",
+        citations: 51,
+      },
+      {
+        title:
+          "Double doped cerium-based superhydrophobic layered double hydroxide protective films grown on anodic aluminium surface",
+        journal: "Journal of Alloys and Compounds",
+        year: "2020",
+        doi: "10.1016/j.jallcom.2020.156112",
+        citations: 39,
+      },
+      {
+        title:
+          "MgAl-LDH/graphene protective film: Insight into LDH-graphene interaction",
+        journal: "Surface and Coatings Technology",
+        year: "2020",
+        doi: "10.1016/j.surfcoat.2020.126253",
+        citations: 34,
+      },
+      {
+        title:
+          "Protective Cerium-Based Layered Double Hydroxides Thin Films Developed on Anodized AA6082",
+        journal: "Advances in Materials Science and Engineering",
+        year: "2020",
+        doi: "10.1155/2020/5785393",
+        citations: 27,
+      },
+      {
+        title:
+          "Effect of operating parameters on the structural growth of ZnAl layered double hydroxide on AA6082 and corresponding corrosion resistance properties",
+        journal: "Journal of Coatings Technology and Research",
+        year: "2019",
+        doi: "10.1007/s11998-019-00291-6",
+        citations: 26,
+      },
+      {
+        title:
+          "Fire retardancy of aluminum hydroxide reinforced flame retardant modified epoxy resin composite",
+        journal: "Russian Journal of Applied Chemistry",
+        year: "2018",
+        doi: "10.1134/S1070427218040201",
+        citations: 25,
+      },
+      {
+        title:
+          "Chlorides entrapment capability of various in-situ grown NiAl-LDHs: Structural and corrosion resistance properties",
+        journal: "Coatings",
+        year: "2020",
+        doi: "10.3390/coatings10040384",
+        citations: 20,
+      },
+      {
+        title:
+          "Synthesis of novel cone-shaped CaAl-LDH directly on aluminum alloy by a facile urea hydrolysis method",
+        journal: "SN Applied Sciences",
+        year: "2019",
+        doi: "10.1007/s42452-019-1461-9",
+        citations: 17,
+      },
+      {
+        title:
+          "Ordering and disordering of in situ grown MgAl-layered double hydroxide and its effect on the structural and corrosion resistance properties",
+        journal: "International Journal of Minerals, Metallurgy and Materials",
+        year: "2019",
+        doi: "10.1007/s12613-019-1876-8",
+        citations: 16,
+      },
+      {
+        title:
+          "Sorption of As(V) from aqueous solution using in situ growth MgAl-NO3 layered double hydroxide thin film developed on AA6082",
+        journal: "SN Applied Sciences",
+        year: "2019",
+        doi: "10.1007/s42452-019-1051-x",
+        citations: 12,
+      },
+      {
+        title:
+          "Role of anodic precursor layer thickness on PEO coatings: Energy consumption and long-term corrosion performance",
+        journal: "Surface and Coatings Technology",
+        year: "2024",
+        doi: "10.1016/j.surfcoat.2024.130186",
+        citations: 5,
+      },
+      {
+        title:
+          "Ce-Doped-MgAl Superhydrophobic Layered Double Hydroxide for Enhanced Corrosion Resistance Properties",
+        journal: "Solids",
+        year: "2021",
+        doi: "10.3390/solids2010005",
+        citations: 5,
+      },
+    ],
     contact: {
       email: "miqbal@ucm.es",
       googleScholar:
@@ -49,17 +165,6 @@
       orcid: "0000-0002-1733-2744",
     },
   };
-
-  fetchCitations(rssFeedUrl)
-    .then((data) => {
-      researcher.publications = data;
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-    .finally(() => {
-      isLoading = false;
-    });
 </script>
 
 <main class="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
@@ -134,15 +239,6 @@
       <h2 class="text-2xl font-semibold text-gray-900 dark:text-white mb-4">
         Publications
       </h2>
-      {#if isLoading}
-        <div class="text-center">
-          <span class="loader"></span>
-        </div>
-      {:else if researcher.publications.length === 0}
-        <p class="text-center text-gray-700 dark:text-gray-300">
-          No publications found.
-        </p>
-      {/if}
       <div class="space-y-4">
         {#each researcher.publications as pub}
           <div class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-sm">
@@ -150,11 +246,11 @@
               {pub.title}
             </h3>
             <p class="text-gray-600 dark:text-gray-300">
-              {pub.journal}
+              {pub.journal}, {pub.year}
             </p>
             <div class="flex justify-between items-center mt-2">
               <p class="text-gray-500 dark:text-gray-400 text-sm">
-                DOI: {pub.doi ?? "N/A"}
+                DOI: {pub.doi}
               </p>
               <span
                 class="text-primary-600 dark:text-primary-400 text-sm font-medium"
