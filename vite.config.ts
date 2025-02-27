06d9b5a01d7f548b32be4be6af9cf467abd892b7
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2025-02-27 18:48:14 +0500

    Revert "fix: build issues in vite config"
    This reverts commit 9d064dd1f76deaa9d58c884cfb8161221dcb509c.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-console.log("base url VITE_BASE => ", process.env.VITE_BASE);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.VITE_BASE || '/',
+  base: "/researcher-page-template/"
 })
